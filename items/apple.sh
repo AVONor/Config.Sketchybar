f49491a8b573c396d85ad12a89e132bc19561a3a
AVONor <gretelclara28@gmail.com>
2025-04-13 11:27:23 -0600

    Corrección de tamaño logo e idoma de las etiquetas

--- items/apple.sh
+++ items/apple.sh
@@ -2,7 +2,7 @@
 
 sketchybar    --add item apple.logo left                                                      \
               --set apple.logo icon=􀣺                                                         \
-                               icon.font="SF Pro:Black:12.0"                                  \
+                               icon.font="SF Pro:Black:14.0"                                  \
                                icon.background.drawing=on \
                                icon.border_width=5 \
                                icon.border_color=$WHITE \
@@ -15,16 +15,16 @@ sketchybar    --add item apple.logo left                                        
                                                                                       \
               --add item apple.preferences popup.apple.logo                                   \
               --set apple.preferences icon=􀺽                                                  \
-                               label="Preferences"                                            \
+                               label="Ajustes del sistema"                                            \
                                click_script="open -a 'System Preferences';                    
                                              sketchybar -m --set apple.logo popup.drawing=off"\
               --add item apple.activity popup.apple.logo                                      \
               --set apple.activity icon=􀒓                                                     \
-                               label="Activity"                                               \
+                               label="Monitor de actividad"                                               \
                                click_script="open -a 'Activity Monitor';                       
                                              sketchybar -m --set apple.logo popup.drawing=off"\
               --add item apple.lock popup.apple.logo                                          \
               --set apple.lock icon=􀒳                                                         \
-                               label="Lock Screen"                                            \
+                               label="Reposo"                                            \
                                click_script="pmset displaysleepnow;                           
                                              sketchybar -m --set apple.logo popup.drawing=off"
